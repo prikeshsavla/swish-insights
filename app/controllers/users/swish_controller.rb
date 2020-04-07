fc9beef5e5c8131bdd9a5bcb4f6d9dbc6630e71a
prikesh.savla <prikesh.savla@external.swiggy.in>
2020-04-07 21:10:06 +0530

    Save swishes and add index page to view all swishes

--- app/controllers/users/swish_controller.rb
+++ app/controllers/users/swish_controller.rb
@@ -1,11 +1,25 @@
 class Users::SwishController < Users::BaseController
   def index
-      print(current_user)
-
+    @swishes = Swish.all.where(user: current_user)
   end
 
   def new
 
+  end
+
+  def create
+    swish = Swish.new(url: params[:swish][:url])
+    swish.user = current_user
+    swish.data = params[:swish][:data]
+    params[:swish][:categories].each do |category|
+      swish.swish_categories.build(category: Category.find_by_value(category))
+    end
+    swish.save!
+    print params[:swish][:url]
+    print swish.url
+    print swish
+
+    render json: {"value": 2}, status: 200
   end
 
 
--- app/javascript/packs/swish.js
+++ app/javascript/packs/swish.js
@@ -7,6 +7,7 @@
 
 import Vue from 'vue'
 
+
 document.addEventListener('DOMContentLoaded', () => {
     var app = new Vue({
 
@@ -38,7 +39,6 @@ document.addEventListener('DOMContentLoaded', () => {
                 this.loadingResults = true;
                 const url = this.setUpQuery();
                 console.log("Run: " + url);
-                //this.findLinks();
 
                 const that = this;
                 fetch(url)
@@ -61,6 +61,26 @@ document.addEventListener('DOMContentLoaded', () => {
                             lighthouse.audits["estimated-input-latency"].displayValue
                         };
                         that.showLighthouseContent(lighthouseMetrics);
+                        $.ajaxSetup({
+                            beforeSend: function (xhr) {
+                                xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))
+                            },
+                        });
+                        $.ajax({
+                            url: "/users/swish",
+                            type: 'POST',
+
+                            data: {
+                                "swish": {
+                                    url: that.url,
+                                    data: lighthouseMetrics,
+                                    categories: that.categories
+                                }
+                            },
+                            success: console.log
+                        });
+
+
                         that.loadingResults = false;
                     });
             },
@@ -111,59 +131,4 @@ document.addEventListener('DOMContentLoaded', () => {
 
     });
 
-    console.log(app)
-})
-
-
-// The above code uses Vue without the compiler, which means you cannot
-// use Vue to target elements in your existing html templates. You would
-// need to always use single file components.
-// To be able to target elements in your existing html/erb templates,
-// comment out the above code and uncomment the below
-// Add <%= javascript_pack_tag 'hello_vue' %> to your layout
-// Then add this markup to your html template:
-//
-// <div id='hello'>
-//   {{message}}
-//   <app></app>
-// </div>
-
-
-// import Vue from 'vue/dist/vue.esm'
-// import App from '../app.vue'
-//
-// document.addEventListener('DOMContentLoaded', () => {
-//   const app = new Vue({
-//     el: '#hello',
-//     data: {
-//       message: "Can you say hello?"
-//     },
-//     components: { App }
-//   })
-// })
-//
-//
-//
-// If the project is using turbolinks, install 'vue-turbolinks':
-//
-// yarn add vue-turbolinks
-//
-// Then uncomment the code block below:
-//
-// import TurbolinksAdapter from 'vue-turbolinks'
-// import Vue from 'vue/dist/vue.esm'
-// import App from '../app.vue'
-//
-// Vue.use(TurbolinksAdapter)
-//
-// document.addEventListener('turbolinks:load', () => {
-//   const app = new Vue({
-//     el: '#hello',
-//     data: () => {
-//       return {
-//         message: "Can you say hello?"
-//       }
-//     },
-//     components: { App }
-//   })
-// })
+});
