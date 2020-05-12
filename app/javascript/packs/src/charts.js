bfbbad90f94a459d5a570179f69e31759d6545cb
prikesh.savla <prikesh.savla@external.swiggy.in>
2020-05-12 08:37:35 +0530

    Add id as necessary for heatmap

--- app/javascript/packs/src/charts.js
+++ app/javascript/packs/src/charts.js
@@ -7,27 +7,25 @@ const charts = {
     heatMap() {
         const heatMapSelector = '[data-chart=heatmap]';
         if ($(heatMapSelector).length) {
-
-
             $(heatMapSelector).each(function (index, element) {
                 const id = $(element).attr('id');
-
-                const datapoints = $(element).data('datapoints');
-                const startDate = new Date($(element).data('startAt'));
-                const endDate = new Date($(element).data('endAt'));
-                const data = {
-                    dataPoints: datapoints,
-                    start: startDate, // a JS date object
-                    end: endDate
-                };
-                console.log($(element).data('startAt'));
-                let chart = new Chart('#' + id, {
-                    type: 'heatmap',
-                    data: data,
-                    discreteDomains: 0,
-                    colors: ['#FFE7F3', '#FF70B6', '#FF007D', '#B40058', '#8A0044'],
-                });
-
+                if (id) {
+                    const datapoints = $(element).data('datapoints');
+                    const startDate = new Date($(element).data('startAt'));
+                    const endDate = new Date($(element).data('endAt'));
+                    const data = {
+                        dataPoints: datapoints,
+                        start: startDate, // a JS date object
+                        end: endDate
+                    };
+                    console.log($(element).data('startAt'));
+                    let chart = new Chart('#' + id, {
+                        type: 'heatmap',
+                        data: data,
+                        discreteDomains: 0,
+                        colors: ['#FFE7F3', '#FF70B6', '#FF007D', '#B40058', '#8A0044'],
+                    });
+                }
             });
 
 
